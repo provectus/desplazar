7067e128816ee2756cd7e221c29ffd2335e1b24e
Konstantin <loki.od@gmail.com>
2012-04-25 01:58:14 +0300

    Add all industry

---
+++ presentation/js/common.js
@@ -0,0 +1,32 @@
+var myScroll;
+function loaded() {
+	myScroll = new iScroll('iscroll');
+}
+//document.addEventListener('DOMContentLoaded', loaded, false);
+
+         var toggle;
+         $(function() {
+
+             toggle = function() {
+ 				var ele = document.getElementById("more-details");
+  			if(ele.style.display == "block") {
+                     ele.style.display = "none";
+                     app.bind();
+		    	} else {
+                     ele.style.display = "block";
+                     app.unbind();
+ 				}
+  		};
+
+             var app = new Desplazar($('#canvas'), { direction: Desplazar.VERTICAL });
+             //app.bind();
+             new iScroll('iscroll');
+
+             //$('#canvas > ul > li').each(function(idx, item) {
+             //    new Desplazar($(item), { direction: Desplazar.HORIZONTAL });
+             //});
+	$('#publicy-held-company a').lightBox({
+		imageBtnPrev: 'assets/gallery/prev_min.png',
+		imageBtnNext: 'assets/gallery/next_min.png'
+	});
+});
